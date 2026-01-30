8efb19a7eb9655a9bf9008853a4b1438828e0a9b
Luke Hann <lukehann@Krispy.local>
2026-01-30 11:58:06 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="flex h-full bg-gray-800 items-center justify-center text-white">
+        Portfolio
+      </div>
+    </>
+  )
+}
+
+export default App
